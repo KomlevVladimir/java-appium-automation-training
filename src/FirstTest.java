0d738adf79d6cacb91b3e858288626606ab84b5e
KomlevVladimir <roastr@gmail.com>
2019-11-17 00:01:06 +0300

    home work 2-1 is done

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,15 +1,32 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FirstTest {
     private AppiumDriver driver;
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -27,8 +44,19 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest() {
-        System.out.println("First test run");
+    public void checkThatSearchInputContainsTextSearchTest() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Could not find 'Search Wikipedia' input",
+                5
+        );
+        String actualResult = waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+                "Could not find search input",
+                5
+        ).getText();
+
+        Assert.assertEquals("Search input does not contain 'Search…' text", "Search…", actualResult);
     }
 
     @After
