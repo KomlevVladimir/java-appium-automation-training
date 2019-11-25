f0f2c59f658c38ea954ec34d5ef451ae7cb7243f
KomlevVladimir <roastr@gmail.com>
2019-11-26 02:22:14 +0300

    home work 3-1 is done

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -13,7 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.By.xpath;
 
 public class FirstTest {
@@ -37,6 +37,12 @@ public class FirstTest {
         return element;
     }
 
+    private WebElement waitElementAndClear(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
     @Before
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -53,8 +59,10 @@ public class FirstTest {
     }
 
     @Test
-    public void checkThatEveryTitleContainsSearchTextTest() {
+    public void saveTwoArticlesTest() {
         String text = "Java";
+        String folderName = "Java folder";
+
         waitForElementAndClick(
                 xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Could not find 'Search Wikipedia' input",
@@ -66,14 +74,148 @@ public class FirstTest {
                 "Could not find search input",
                 5
         );
-        List<WebElement> titles = driver.findElementsByXPath(
-                "//*[@resource-id='org.wikipedia:id/page_list_item_title']"
+        waitForElementAndClick(
+                xpath("//*[@text='Island of Indonesia']"),
+                "Could find 'Isalnd of Indonesia' article",
+                5
+        );
+        waitForElementPresent(
+                xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text']"),
+                "Could not find article title",
+                15
         );
 
-        titles.forEach(t -> assertTrue(
-                text + " word is absent in the " + t.getText() + " title",
-                t.getText().contains(text)
-        ));
+        waitForElementAndClick(
+                xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Could not find the 'More options' element",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='Add to reading list']"),
+                "Could not find the 'Add to reading list' option",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='GOT IT']"),
+                "Could not find the 'GOT IT' button",
+                5
+        );
+        waitElementAndClear(
+                xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+                "Could not find name of the list input",
+                5
+        );
+        waitElementAndSendKeys(xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+                folderName,
+                "Could not find name of the list input",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='OK']"),
+                "Could not find the 'OK' button",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@content-desc='Navigate up']"),
+                "Could not find a close button",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Could not find 'Search Wikipedia' input",
+                5
+        );
+        waitElementAndSendKeys(
+                xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+                text,
+                "Could not find search input",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='Object-oriented programming language']"),
+                "Could find 'Object-oriented programming language",
+                5
+        );
+        waitForElementPresent(
+                xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text']"),
+                "Could not find article title",
+                15
+        );
+        waitForElementAndClick(
+                xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Could not find the 'More options' element",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='Add to reading list']"),
+                "Could not find the 'Add to reading list' option",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='" + folderName + "']"),
+                "Could not find the" + folderName,
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@content-desc='Navigate up']"),
+                "Could not find a close button",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@content-desc='My lists']"),
+                "Could not find the 'My lists' button",
+                5
+        );
+        waitForElementAndClick(
+                xpath("//*[@text='" + folderName + "']"),
+                "Could not find" + folderName,
+                5
+        );
+        waitForElementPresent(
+                xpath("//*[@text='" + folderName + "']"),
+                "Could not load " + folderName,
+                5
+        );
+        List<WebElement> savedArticlesBefore =
+                driver.findElementsByXPath("//*[@resource-id='org.wikipedia:id/page_list_item_container']");
+        savedArticlesBefore.get(0).findElement(xpath("//*[@content-desc='More options']")).click();
+        waitForElementAndClick(
+                xpath("//*[@text='Remove from " + folderName + "']"),
+                "Could not find a remove button",
+                5
+        );
+        List<WebElement> savedArticlesAfter =
+                driver.findElementsByXPath("//*[@resource-id='org.wikipedia:id/page_list_item_container']");
+
+        assertEquals("the number of articles is not equal to 1", 1, savedArticlesAfter.size());
+
+        String titleInFolder = savedArticlesAfter
+                .get(0)
+                .findElement(xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"))
+                .getText();
+
+        assertEquals(
+                "The title in folder is not " + titleInFolder,
+                "Java (programming language)",
+                titleInFolder
+        );
+
+        waitForElementAndClick(
+                xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "Could not file the article title",
+                5
+        );
+        String actualTitle = waitForElementPresent(
+                xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text']"),
+                "Could not find the actual article title",
+                15
+        ).getText();
+
+        assertEquals(
+                actualTitle + " is not equal to 'Java (programming language)'",
+                "Java (programming language)",
+                actualTitle
+        );
     }
 
     @After
