8337ac14195d277de7d454264b7978c60637e537
KomlevVladimir <roastr@gmail.com>
2020-01-04 22:25:15 +0300

    fix

--- src/test/java/lib/ui/ios/IOSMyListsPageObject.java
+++ src/test/java/lib/ui/ios/IOSMyListsPageObject.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class IOSMyListsPageObject extends MyListsPageObject {
     static {
         ARTICLES = "xpath://XCUIElementTypeCollectionView";
-        ARTICLE_BY_TITLE_TPL = "xpath://XCUIElementTypeLink[@name='{TITLE}']";
+        ARTICLE_BY_TITLE_TPL = "xpath://XCUIElementTypeLink[contains(@name, '{TITLE}')]";
         SYNC_ARTICLES_CLOSE_BUTTON = "xpath://XCUIElementTypeButton[@name='Close']";
         ARTICLE_CONTAINS_TITLE_TPL = "xpath://XCUIElementTypeLink[contains(@name, '{TITLE}')]";
     }
